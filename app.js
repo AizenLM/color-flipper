03f8c5d5472b18270489ff63d1ae401f2b8cce6a
aizenLM <diamnteludwing145@gmail.com>
2024-05-07 19:22:11 -0600

    hex color

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+//  get the button element
+const btn = document.getElementById("btn");
+//Get the color element
+const color = document.querySelector(".color");
+btn.addEventListener("click", () => { 
+    // generate a random number between 0 and 3
+    const randomNumber = Math.floor(Math.random() * colors.length);
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+    
+});
