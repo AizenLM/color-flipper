e0339f238f47e61148774930b84f3106f16d292a
AizenLM <diamanteludwing145@gmail.com>
2024-05-07 20:13:39 -0600

    hexadecimal completed

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 btn.addEventListener("click", () => { 
     // generate a random number between 0 and 3
-    const randomNumber = Math.floor(Math.random() * colors.length);
+    const randomNumber = Math.floor(Math.random() * colors.length); 
     document.body.style.backgroundColor = colors[randomNumber];
     color.textContent = colors[randomNumber];
+    color.style.color = colors[randomNumber];
     
 });
